21c2ff352acf7b58216735f9c2fa007e30afba32
Stefan Scheffler <sscheffler@avantgarde-labs.de>
2014-10-02 00:21:48 +0200

    added mount of temp folder

--- jenkins/src/main/recources/run.sh
+++ jenkins/src/main/recources/run.sh
@@ -9,4 +9,4 @@ M2=.m2/repository
 
 #rebuild and run	 container
 sudo docker build -t=local/jenkins .
-sudo docker run -d -v $HOME/.jenkins:/root/.jenkins -v $HOME/$M2:/root/$M2 -v $HOME/.repository:/data/repository -p 127.0.0.1:8090:8080 --name jenkins local/jenkins
+sudo docker run -d -v $HOME/tmp:/tmp -v $HOME/.jenkins:/root/.jenkins -v $HOME/$M2:/root/$M2 -v $HOME/.repository:/data/repository -p 127.0.0.1:8090:8080 --name jenkins local/jenkins
