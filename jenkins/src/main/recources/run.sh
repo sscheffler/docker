a89acb2eeae4fa62f0d6edbc3744d2cc514fc9cc
Stefan Scheffler <sscheffler@avantgarde-labs.de>
2014-10-01 23:17:44 +0200

    added installation of jdk 7

--- jenkins/src/main/recources/run.sh
+++ jenkins/src/main/recources/run.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+#stop old container
 bash stop.sh
+
+#set environment
 HOME=/home/sscheffler
 M2=.m2/repository
 
+#rebuild and run	 container
 sudo docker build -t=local/jenkins .
 sudo docker run -d -v $HOME/.jenkins:/root/.jenkins -v $HOME/$M2:/root/$M2 -v $HOME/.repository:/data/repository -p 127.0.0.1:8090:8080 --name jenkins local/jenkins
