48de748fb333063bfc169ebf223f5fa5cb8b0d91
Stefan Scheffler <sscheffler@avantgarde-labs.de>
2014-10-01 22:13:58 +0200

    changed mounted folder to global home folder

--- jenkins/src/main/recources/run.sh
+++ jenkins/src/main/recources/run.sh
@@ -3,4 +3,4 @@
 bash stop.sh
 
 sudo docker build -t=local/jenkins .
-sudo docker run -d -v /home/sscheffler/.jenkins/jobs:/root/.jenkins/jobs -v /home/sscheffler/.jenkins/plugins:/root/.jenkins/plugins -p 127.0.0.1:8090:8080 --name jenkins local/jenkins
+sudo docker run -d -v /home/sscheffler/.jenkins:/root/.jenkins -p 127.0.0.1:8090:8080 --name jenkins local/jenkins
