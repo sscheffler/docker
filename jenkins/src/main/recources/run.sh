68bf713b9cf7400e02dd9912237649fad7dbfa95
Stefan Scheffler <sscheffler@avantgarde-labs.de>
2014-10-01 21:13:18 +0200

    separated tomcat from jenkins docker / mounted external job folder

--- jenkins/src/main/recources/run.sh
+++ jenkins/src/main/recources/run.sh
@@ -3,4 +3,4 @@
 bash stop.sh
 
 sudo docker build -t=local/jenkins .
-sudo docker run -d -p 127.0.0.1:8090:8080 --name jenkins local/jenkins
+sudo docker run -d -v /home/sscheffler/.jenkins/jobs:/root/.jenkins/jobs -p 127.0.0.1:8090:8080 --name jenkins local/jenkins
