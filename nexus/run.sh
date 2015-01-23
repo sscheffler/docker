97aac31935fdc75767cf49a3efd3a9e4d61aff19
Stefan Scheffler <sscheffler@avantgarde-labs.de>
2015-01-23 20:53:45 +0100

    added docker for nexus

---
+++ nexus/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#stop old container
+#bash stop.sh
+
+#set environment
+HOME=/home/sscheffler
+M2=.m2/repository
+
+#rebuild and run	 container
+docker build --quiet=false -t=stefanscheffler/nexus .
+#docker run -d -v $HOME/tmp:/tmp -v $HOME/.jenkins:/root/.jenkins -v $HOME/$M2:/root/$M2 -v $HOME/.repository:/data/repository -p 127.0.0.1:8090:8080 --name jenkins stefanscheffler/jenkins
