044b0c0aaab92cb404a5d412dbd88c0842fc9c59
Stefan Scheffler <sscheffler@avantgarde-labs.de>
2015-01-29 14:25:25 +0100

    changed orientdb docker to docker abiyasa/orientdb-2.0

--- orientdb/run.sh
+++ orientdb/run.sh
@@ -8,5 +8,7 @@
 #M2=.m2/repository
 
 #rebuild and run	 container
-sudo docker build --quiet=false -t=stefanscheffler/orientdb . 
-sudo docker run -d --name orientdb stefanscheffler/orientdb
+#sudo docker build --quiet=false -t=abiyasa/orientdb-2.0 . 
+sudo docker stop orientdb
+sudo docker rm orientdb
+sudo docker run -d -p 2424:2424 -p 2480:2480 --name orientdb abiyasa/orientdb-2.0
