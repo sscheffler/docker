8a78bcc5df98b6a63f6fee972267bb3711fb22e8
Stefan Scheffler <sscheffler@avantgarde-labs.de>
2015-02-03 19:33:21 +0100

    adapted Dockerfile and run script with several changes

--- jdk8/run.sh
+++ jdk8/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #stop old container
+bash docker stop jdk8
 bash docker rm jdk8
 
 #set environment
@@ -9,4 +10,4 @@ M2=.m2/repository
 
 #rebuild and run	 container
  docker build --quiet=false -t=stefanscheffler/jdk8 .
-# docker run -d -v $HOME/tmp:/tmp -v $HOME/.jenkins:/root/.jenkins -v $HOME/$M2:/root/$M2 -v $HOME/.repository:/data/repository -p 127.0.0.1:8090:8080 --name jenkins stefanscheffler/jenkins
+ docker run -d  --name jdk8 stefanscheffler/jdk8
