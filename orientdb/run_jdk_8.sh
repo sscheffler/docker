7fb239325412570e761688755d0cec4a1e7c55ec
Stefan Scheffler <sscheffler@avantgarde-labs.de>
2015-02-03 17:28:08 +0100

    added build file for jdk8

---
+++ orientdb/run_jdk_8.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#VARIABLES
+DATA=/data/orientdb
+CONFIG=/etc/orientdb/config
+
+#STOP OLD CONTAINER
+#sudo docker stop orientdb
+#sudo docker rm orientdb
+
+#RUN CONTAINER
+sudo docker build --quiet=false -t=stefanscheffler/orientdb .
+#sudo docker run -d -v $DATA/log:/opt/orientdb/log -v $CONFIG:/opt/orientdb/config -v $DATA/db:/opt/orientdb/databases -v $DATA/backup:/opt/orientdb/backup -p 2424:2424 -p 2480:2480 --name orientdb abiyasa/orientdb-2.0
