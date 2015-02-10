97f37dbe76dd91eda171dd1289c3b045874db1f3
Stefan Scheffler <sscheffler@avantgarde-labs.de>
2015-02-10 18:51:06 +0100

    get port 9200 back, as it is needed from kibana

--- elasticsearch/run.sh
+++ elasticsearch/run.sh
@@ -1,3 +1,3 @@
 sh stop.sh
-docker run -d -v /data:/elasticsearch/data -p 127.0.0.1:8000:8000 --name kibana nshou/elasticsearch-kibana 
+docker run -d -v /data:/elasticsearch/data -p 9200:9200 -p 127.0.0.1:8000:8000 --name kibana nshou/elasticsearch-kibana 
 
