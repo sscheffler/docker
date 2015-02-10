842ab3b6ced2fe3c8b3286da1d7aa22a43ada162
Stefan Scheffler <sscheffler@avantgarde-labs.de>
2015-02-10 18:39:10 +0100

    removed unused ports from kibana

--- elasticsearch/run.sh
+++ elasticsearch/run.sh
@@ -1,3 +1,3 @@
 sh stop.sh
-docker run -d -v /data:/elasticsearch/data -p 127.0.0.1:8000:8000 -p 127.0.0.1:9300:9300 -p 127.0.0.1:9200:9200  --name kibana nshou/elasticsearch-kibana 
+docker run -d -v /data:/elasticsearch/data -p 127.0.0.1:8000:8000 --name kibana nshou/elasticsearch-kibana 
 
