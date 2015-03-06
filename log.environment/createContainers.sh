563383a30e72c70a1571aa9ab4a7f1855259dfdb
Stefan Scheffler <sscheffler@avantgarde-labs.de>
2015-03-06 22:37:32 +0100

    adapted config for logstash

--- log.environment/createContainers.sh
+++ log.environment/createContainers.sh
@@ -16,4 +16,4 @@ CONFIG=/etc/orientdb/config
 echo Creating containers
 docker create -v $DATA/elasticsearch:/elasticsearch/data -p 9200:9200 -p 127.0.0.1:8000:8000 --name kibana nshou/elasticsearch-kibana
 docker create -v $DATA/orientdb/log:/opt/orientdb/log -v $CONFIG:/opt/orientdb/config -v $DATA/orientdb/db:/opt/orientdb/databases -p 2424:2424 -p 2480:2480 --name orientdb sscheffler/orientdb
-docker create -v $HOME/repository/docker/logstash/config:/data/config -v $HOME/temp/logstash/index/:/data/input --name=logstash --link kibana:elastic --link orientdb:orient sscheffler/logstash
+docker create -v $HOME/repository/docker/log.environment/config/logstash:/data/config -v $HOME/temp/logstash/index/:/data/input --name=logstash --link kibana:elastic --link orientdb:orient sscheffler/logstash
