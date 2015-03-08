f27445604adabed784428d705532ff976339b119
Stefan Scheffler <sscheffler@avantgarde-labs.de>
2015-03-08 18:14:58 +0100

    added check for first parameter

--- log.environment/createContainers.sh
+++ log.environment/createContainers.sh
@@ -1,4 +1,7 @@
 
+if [ $1 ] 
+then
+
 echo Stoping containers
 docker stop logstash
 docker stop kibana
@@ -17,3 +20,6 @@ echo Creating containers
 docker create -v $DATA/elasticsearch:/elasticsearch/data -p 9200:9200 -p 127.0.0.1:8000:8000 --name kibana nshou/elasticsearch-kibana
 docker create -v $DATA/orientdb/log:/opt/orientdb/log -v $CONFIG:/opt/orientdb/config -v $DATA/orientdb/db:/opt/orientdb/databases -p 2424:2424 -p 2480:2480 --name orientdb sscheffler/orientdb
 docker create -v $DATA/logstash/sincedb:/data/since_db  -v $HOME/repository/docker/log.environment/config/logstash:/data/config -v $HOME/temp/logstash/index/:/data/input --name=logstash --link kibana:elastic --link orientdb:orient sscheffler/logstash
+else
+  echo "HOME not set"
+fi
