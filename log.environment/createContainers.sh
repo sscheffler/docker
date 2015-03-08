84db78f3a2889b1e30f71b241b6528ae7667b1e3
Stefan Scheffler <sscheffler@avantgarde-labs.de>
2015-03-08 18:10:28 +0100

    added sincedb config

--- log.environment/createContainers.sh
+++ log.environment/createContainers.sh
@@ -9,11 +9,11 @@ docker rm logstash
 docker rm kibana
 docker rm orientdb
 
-HOME=/home/sscheffler
+HOME=$1
 DATA=/data
 CONFIG=$HOME/repository/docker/log.environment/config/orientdb
 
 echo Creating containers
 docker create -v $DATA/elasticsearch:/elasticsearch/data -p 9200:9200 -p 127.0.0.1:8000:8000 --name kibana nshou/elasticsearch-kibana
 docker create -v $DATA/orientdb/log:/opt/orientdb/log -v $CONFIG:/opt/orientdb/config -v $DATA/orientdb/db:/opt/orientdb/databases -p 2424:2424 -p 2480:2480 --name orientdb sscheffler/orientdb
-docker create -v $HOME/repository/docker/log.environment/config/logstash:/data/config -v $HOME/temp/logstash/index/:/data/input --name=logstash --link kibana:elastic --link orientdb:orient sscheffler/logstash
+docker create -v $DATA/logstash/sincedb:/data/since_db  -v $HOME/repository/docker/log.environment/config/logstash:/data/config -v $HOME/temp/logstash/index/:/data/input --name=logstash --link kibana:elastic --link orientdb:orient sscheffler/logstash
