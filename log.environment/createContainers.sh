ab0893e5bb7ccf9f42699da915709a61b9619b33
Stefan Scheffler <sscheffler@avantgarde-labs.de>
2015-03-11 06:59:34 +0100

    removed orientdb from environment

--- log.environment/createContainers.sh
+++ log.environment/createContainers.sh
@@ -5,21 +5,17 @@ then
 echo Stoping containers
 docker stop logstash
 docker stop kibana
-docker stop orientdb
 
 echo Removing containers
 docker rm logstash
 docker rm kibana
-docker rm orientdb
 
 HOME=$1
 DATA=/data
-CONFIG=$HOME/repository/docker/log.environment/config/orientdb
 
 echo Creating containers
 docker create -v $DATA/elasticsearch:/elasticsearch/data -p 9200:9200 -p 127.0.0.1:8000:8000 --name kibana nshou/elasticsearch-kibana
-docker create -v $DATA/orientdb/log:/opt/orientdb/log -v $CONFIG:/opt/orientdb/config -v $DATA/orientdb/db:/opt/orientdb/databases -p 2424:2424 -p 2480:2480 --name orientdb sscheffler/orientdb
-docker create -v $DATA/logstash/sincedb:/data/since_db  -v $HOME/repository/docker/log.environment/config/logstash:/data/config -v $HOME/temp/logstash/index/:/data/input --name=logstash --link kibana:elastic --link orientdb:orient sscheffler/logstash
+docker create -v $DATA/logstash/sincedb:/data/since_db  -v $HOME/repository/docker/log.environment/config/logstash:/data/config -v $HOME/temp/logstash/index/:/data/input --name=logstash --link kibana:elastic sscheffler/logstash
 else
   echo "HOME not set"
 fi
