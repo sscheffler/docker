f8c22b131cd6d77a178a353c04e35868414d5386
Stefan Scheffler <sscheffler@avantgarde-labs.de>
2015-03-14 10:32:21 +0100

    add: run.sh

--- java/build.sh
+++ java/build.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-#stop old container
-docker stop jdk8
-docker rm jdk8
-
-#set environment
-HOME=/home/sscheffler
-M2=.m2/repository
 
 #rebuild and run	 container
- docker build --quiet=false -t=stefanscheffler/jdk8 .
- docker run -d  --name jdk8 stefanscheffler/jdk8
+ docker build --quiet=false --force-rm=true -t=stefanscheffler/jdk .
+# docker run -t -i  stefanscheffler/jdk /bin/bash
