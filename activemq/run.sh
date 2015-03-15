808ba9e92ac6e37636bc2e2c53847a50a833ec72
Stefan Scheffler <sscheffler@avantgarde-labs.de>
2015-03-15 09:38:31 +0100

    fixed bug in run script

--- activemq/run.sh
+++ activemq/run.sh
@@ -1,1 +1,8 @@
-docker run -td --name=activemq sscheffler/activemq
+docker stop activemq
+docker rm activemq
+
+docker run \
+	-td \
+	-p 127.0.0.1:8161:8161 \
+	 --name=activemq \
+	sscheffler/activemq
